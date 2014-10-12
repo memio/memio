135d3c3c5df4715ca27374dfdfa98a8bc50dfcaa
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-10-12 22:46:29 +0100

    Added license

--- medio.php
+++ medio.php
@@ -1,6 +1,15 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * This file is part of the Medio project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once __DIR__.'/vendor/autoload.php';
 
 use Gnugat\Medio\Container;
--- test/InjectDependencyTest.php
+++ test/InjectDependencyTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Medio project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace test\Gnugat\Medio;
 
 use test\Gnugat\Medio\Helper\Input;
