555b63d4566a915c18aa5e5679cf3a05988bbbb9
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-01-22 19:25:22 +0000

    Created ArgumentFactory

---
+++ src/Gnugat/Medio/Service/ArgumentFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Gnugat\Medio\Service;
+
+use Gnugat\Medio\Model\Argument;
+
+class ArgumentFactory
+{
+    /**
+     * @param string $type
+     *
+     * @return Argument
+     */
+    public function makeFromType($type)
+    {
+        $nonObjectTypes = array('array', 'boolean', 'callable', 'double', 'integer', 'NULL', 'resource', 'string');
+        if (in_array($type, $nonObjectTypes, true)) {
+            return new Argument($type, 'argument');
+        }
+        if ($type[0] !== '\\') {
+            $type = '\\'.$type;
+        }
+        $nameSpaces = explode('\\', $type);
+        $className = end($nameSpaces);
+        $name = lcfirst($className);
+
+        return new Argument($type, $name, true);
+    }
+
+    /**
+     * @param mixed $variable
+     *
+     * @return Argument
+     */
+    public function makeFromVariable($variable)
+    {
+        if (is_object($variable)) {
+            return $this->makeFromType(get_class($variable));
+        }
+        if (is_callable($variable)) {
+            return $this->makeFromType('callable');
+        }
+
+        return $this->makeFromType(gettype($variable));
+    }
+}
