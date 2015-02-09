b221af48a9c418a9b67404be71cfbe03211060c2
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-02-09 15:33:11 +0000

    Created GeneratingArgumentTest

---
+++ config/phpdi.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'medio.templates' => __DIR__.'/../templates',
+
+    'Twig_Loader_Filesystem' => DI\Object()
+        ->constructor(DI\link('medio.templates')),
+
+    'Twig_Environment' => DI\Object()
+        ->constructor(DI\link('Twig_Loader_Filesystem')),
+);
---
+++ examples/GeneratingArgumentTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Gnugat\Medio\Examples;
+
+use Gnugat\Medio\Model\Argument;
+use Gnugat\Medio\Model\Type;
+
+class GeneratingArgumentTest extends PrettyPrinterTestCase
+{
+    public function testNonTypeHinted()
+    {
+        $argument = new Argument(new Type('string'), 'filename');
+
+        $generatedCode = $this->prettyPrinter->generateCode($argument);
+
+        $this->assertSame('$filename', $generatedCode);
+    }
+
+    public function testArray()
+    {
+        $argument = new Argument(new Type('array'), 'parameters');
+
+        $generatedCode = $this->prettyPrinter->generateCode($argument);
+
+        $this->assertSame('array $parameters', $generatedCode);
+    }
+
+    public function testCallable()
+    {
+        $argument = new Argument(new Type('callable'), 'factory');
+
+        $generatedCode = $this->prettyPrinter->generateCode($argument);
+
+        $expectedCode = (version_compare(PHP_VERSION, '5.4.0') >= 0 ? 'callable $factory' : '$factory');
+        $this->assertSame($expectedCode, $generatedCode);
+    }
+
+    public function testObject()
+    {
+        $argument = new Argument(new Type('\\DateTime'), 'dateTime');
+
+        $generatedCode = $this->prettyPrinter->generateCode($argument);
+
+        $this->assertSame('\\DateTime $dateTime', $generatedCode);
+    }
+}
