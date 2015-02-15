4e2346e50b1b09efeb981c14bb28210127f1b88b
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-02-15 09:56:48 +0000

    Added missing license headers

--- examples/PrettyPrinterTestCase.php
+++ examples/PrettyPrinterTestCase.php
@@ -1,8 +1,18 @@
 <?php
 
+/*
+ * This file is part of the Medio project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gnugat\Medio\Examples;
 
 use DI\ContainerBuilder;
+use Inject;
 use PHPUnit_Framework_TestCase;
 
 class PrettyPrinterTestCase extends PHPUnit_Framework_TestCase
