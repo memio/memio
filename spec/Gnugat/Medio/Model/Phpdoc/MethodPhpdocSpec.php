5c9d11004cfcdfeae991b2ff669c40ac65824dc7
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-03-09 08:05:32 +0000

    Created MethodPhpdoc

---
+++ spec/Gnugat/Medio/Model/Phpdoc/MethodPhpdocSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Medio project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Gnugat\Medio\Model\Phpdoc;
+
+use Gnugat\Medio\Model\Phpdoc\ApiTag;
+use Gnugat\Medio\Model\Phpdoc\Description;
+use Gnugat\Medio\Model\Phpdoc\DeprecationTag;
+use PhpSpec\ObjectBehavior;
+
+class MethodPhpdocSpec extends ObjectBehavior
+{
+    function it_can_be_empty()
+    {
+        $this->isEmpty()->shouldBe(true);
+    }
+
+    function it_can_have_description(Description $description)
+    {
+        $this->setDescription($description);
+        $this->getDescription()->shouldBe($description);
+        $this->isEmpty(false);
+    }
+
+    function it_can_be_tagged_as_api(ApiTag $apiTag)
+    {
+        $this->setApiTag($apiTag);
+        $this->getApiTag()->shouldBe($apiTag);
+        $this->isEmpty()->shouldBe(false);
+    }
+
+    function it_can_be_tagged_as_deprecated(DeprecationTag $deprecationTag)
+    {
+        $this->setDeprecationTag($deprecationTag);
+        $this->getDeprecationTag()->shouldBe($deprecationTag);
+        $this->isEmpty()->shouldBe(false);
+    }
+}
---
+++ src/Gnugat/Medio/Model/Phpdoc/MethodPhpdoc.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * This file is part of the Medio project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gnugat\Medio\Model\Phpdoc;
+
+/**
+ * @api
+ */
+class MethodPhpdoc
+{
+    /**
+     * @var ApiTag
+     */
+    private $apiTag;
+
+    /**
+     * @var DeprecationTag
+     */
+    private $deprecationTag;
+
+    /**
+     * @var Description
+     */
+    private $description;
+
+    /**
+     * @return MethodPhpdoc
+     *
+     * @api
+     */
+    public static function make()
+    {
+        return new self();
+    }
+
+    /**
+     * @param ApiTag $apiTag
+     *
+     * @return MethodPhpdoc
+     *
+     * @api
+     */
+    public function setApiTag(ApiTag $apiTag)
+    {
+        $this->apiTag = $apiTag;
+
+        return $this;
+    }
+
+    /**
+     * @param Description $description
+     *
+     * @return MethodPhpdoc
+     *
+     * @api
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @param DeprecationTag $deprecationTag
+     *
+     * @return MethodPhpdoc
+     *
+     * @api
+     */
+    public function setDeprecationTag(DeprecationTag $deprecationTag)
+    {
+        $this->deprecationTag = $deprecationTag;
+
+        return $this;
+    }
+
+    /**
+     * @return ApiTag
+     */
+    public function getApiTag()
+    {
+        return $this->apiTag;
+    }
+
+    /**
+     * @return Description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return Deprecation
+     */
+    public function getDeprecationTag()
+    {
+        return $this->deprecationTag;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        $hasApiTag = (null !== $this->apiTag);
+        $hasDescription = (null !== $this->description);
+        $hasDeprecationTag = (null !== $this->deprecationTag);
+
+        return !$hasApiTag && !$hasDescription && !$hasDeprecationTag;
+    }
+}
